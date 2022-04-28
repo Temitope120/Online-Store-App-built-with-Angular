fd2ad4bcec9fab6474845635a8c1b3454bd9edfe
Funminiyi Adedoyin <doyinfummi2012@gmail.com>
2022-04-28 14:02:48 +0100

    Routing

--- add-to-cart/src/app/app-routing.module.ts
+++ add-to-cart/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { CartComponent } from './component/cart/cart.component';
+import { ProductComponent } from './component/product/product.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: "", redirectTo: 'products', pathMatch: 'full'},
+  {path: 'products', component: ProductComponent},
+  {path: 'cart', component: CartComponent},
+ 
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
