d2b3752e4b51fbdb8d567407f1be83651796664d
Funminiyi Adedoyin <doyinfummi2012@gmail.com>
2022-04-28 13:53:16 +0100

    displaying products from an API

---
+++ add-to-cart/src/app/component/product/product.component.ts
@@ -0,0 +1,19 @@
+import { ApiService } from './../../service/api.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+  public productList : any;
+  constructor(private api:ApiService) { }
+
+  ngOnInit(): void {
+    this.api.getProduct().subscribe(res=>{
+      this.productList = res;
+    })
+  }
+
+}
---
+++ add-to-cart/src/app/service/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+// url = "https://fakestoreapi.com/products";
+  constructor(private http : HttpClient) { }
+
+  getProduct() {
+    return this.http.get<any>("https://fakestoreapi.com/products").pipe(map((res:any)=> {
+      return res;
+    }))
+  }
+}
